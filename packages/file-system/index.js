442b957fccdc2e6fe860b702d3bb4647d40ca458
Itzikgabay <itzikdevio@gmail.com>
2023-01-15 23:15:13 +0200

    Add `file-system` package with `copyFolder` and `writeFile` functions

---
+++ packages/file-system/index.js
@@ -0,0 +1,27 @@
+import { cp, writeFile } from 'node:fs/promises';
+import logs from "../logs/index.js";
+
+export class FileSystem {
+    constructor() {
+    }
+
+    async copyFolder(source, destination) {
+        try {
+            await cp(source, destination, { recursive: true });
+            return true;
+        } catch (error) {
+            logs.warning(error);
+            return false;
+        }
+    }
+
+    async writeFile(path, content) {
+        try {
+            await writeFile(path, content);
+            return true;
+        } catch (error) {
+            logs.warning(error);
+            return false;
+        }
+    }
+}
