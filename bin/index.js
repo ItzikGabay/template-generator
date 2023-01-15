671d2d11290e8f67720195c442309fdad4a848a9
Itzikgabay <itzikdevio@gmail.com>
2023-01-16 00:00:11 +0200

    Small flow refactor for `index`

--- bin/index.js
+++ bin/index.js
@@ -2,33 +2,24 @@
 
 import logs from "../packages/logs/index.js";
 import errors from "../packages/utils/errors.js";
-import config from "../packages/config/index.js";
 import FileSystem from "../packages/file-system/index.js";
-
-const isDebugMode = config.isDebugMode;
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 // template-generator [name] [path] [options]
 (async () => {
-    // Removes first two arguments
-    // (first is node, the second is the file path)
-    const inputArguments = process.argv.slice(2);
-    let [name, path, type, ...options] = inputArguments;
-    const fse = new FileSystem();
-    const currentPath = process.cwd();
+  // Removes first two arguments
+  // (first is node, the second is the file path)
+  const [type] = process.argv.slice(2);
 
-    if(!name) {
-        return logs.warning(errors.NO_NAME_PROVIDED);
-    }
+  const fse = new FileSystem();
+  const processPath = process.cwd();
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const folderPath = path.resolve(__dirname, "../templates");
 
-    if(!path) {
-        path = './';
-    }
+  if (!type) {
+    return logs.warning(errors.NO_TYPE_PROVIDED);
+  }
 
-    if(!type) {
-        type = 'base'
-    }
-
-    await fse.copyFolder(`${currentPath}/templates/${type}`, `${currentPath}`)
-
-    isDebugMode && console.log(inputArguments);
+  await fse.copyFolder(`${folderPath}/${type}`, processPath);
 })();
