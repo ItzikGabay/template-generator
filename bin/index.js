854201dd994b9ba2e3a3b8863d60de2de2c97a3d
Itzikgabay <itzikdevio@gmail.com>
2023-01-15 22:59:12 +0200

    Add debugMode, and simple validation for name and path

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,25 @@
 #! /usr/bin/env node
 
 import logs from "../packages/logs/index.js";
+import errors from "../packages/utils/errors.js";
+import config from "../packages/config/index.js";
 
+const isDebugMode = config.isDebugMode;
+
+// template-generator [name] [path] [options]
 (async () => {
     // Removes first two arguments
     // (first is node, the second is the file path)
     const inputArguments = process.argv.slice(2);
+    let [name, path, ...options] = inputArguments;
 
-    if(!inputArguments.length) {
-        return logs.warning('Error!')
+    if(!name) {
+        return logs.warning(errors.NO_NAME_PROVIDED)
     }
 
-    console.log(inputArguments);
+    if(!path) {
+        path = './';
+    }
+
+    isDebugMode && console.log(inputArguments);
 })();
