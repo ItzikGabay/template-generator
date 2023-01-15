136a916bfd43723d3827f3494982b49fb4c64dde
Itzikgabay <itzikdevio@gmail.com>
2023-01-15 22:52:08 +0200

    Basic logic for index

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,15 @@
 #! /usr/bin/env node
 
-console.log("works");
+import logs from "../packages/logs/index.js";
+
+(async () => {
+    // Removes first two arguments
+    // (first is node, the second is the file path)
+    const inputArguments = process.argv.slice(2);
+
+    if(!inputArguments.length) {
+        return logs.warning('Error!')
+    }
+
+    console.log(inputArguments);
+})();
