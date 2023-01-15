db2da4c855cfbccd3ba36286db9b58c1717de46d
Itzikgabay <itzikdevio@gmail.com>
2023-01-16 00:05:06 +0200

    Catching `ENOENT` error

--- packages/file-system/index.js
+++ packages/file-system/index.js
@@ -1,27 +1,31 @@
-import { cp, writeFile } from 'node:fs/promises';
+import { cp, writeFile } from "node:fs/promises";
 import logs from "../logs/index.js";
+import errors from "../utils/errors.js";
 
 export default class FileSystem {
-    constructor() {
-    }
+  constructor() {}
 
-    async copyFolder(source, destination) {
-        try {
-            await cp(source, destination, { recursive: true });
-            return true;
-        } catch (error) {
-            logs.warning(error);
-            return false;
-        }
+  async copyFolder(source, destination) {
+    try {
+      await cp(source, destination, { recursive: true });
+      return true;
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        logs.warning(errors.NO_SUCH_FILE_OR_DIRECTORY);
+      } else {
+        logs.warning(error);
+      }
+      return false;
     }
+  }
 
-    async writeFile(path, content) {
-        try {
-            await writeFile(path, content);
-            return true;
-        } catch (error) {
-            logs.warning(error);
-            return false;
-        }
+  async writeFile(path, content) {
+    try {
+      await writeFile(path, content);
+      return true;
+    } catch (error) {
+      logs.warning(error);
+      return false;
     }
+  }
 }
--- packages/utils/errors.js
+++ packages/utils/errors.js
@@ -1,3 +1,4 @@
 export default Object.freeze({
-    NO_TYPE_PROVIDED: 'No type provided',
-})
+  NO_TYPE_PROVIDED: "No type provided",
+  NO_SUCH_FILE_OR_DIRECTORY: "No such file or directory",
+});
