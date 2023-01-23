2ec37f68eae8085ef610f22572b85653bd9e2fba
Itzik Gabay <itzikdevio@gmail.com>
2023-01-23 23:50:22 +0200

    chore: Add git mode variable

--- bin/index.js
+++ bin/index.js
@@ -2,19 +2,19 @@
 
 import logs from "../packages/logs/index.js";
 import errors from "../packages/utils/errors.js";
-import {copyFolderByType, gitClone} from "../packages/file-system/index.js";
+import { copyFolderByType, gitClone } from "../packages/file-system/index.js";
 import config from "../packages/config/index.js";
-
 
 (async () => {
   // Removes the first two arguments
   const [template, repoPath] = process.argv.slice(2);
+  const isGitMode = template === "--git";
 
-  if (!template || !config.availableTemplates[template]) {
+  if (!template || (!config.availableTemplates[template] && !isGitMode)) {
     return logs.warning(errors.NO_TYPE_PROVIDED);
   }
 
-  if(template === '--git') {
+  if (isGitMode) {
     return await gitClone(repoPath);
   }
 
